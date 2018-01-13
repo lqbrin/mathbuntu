3b7ce6041fa790cbbe99e2fb6d8e94eb62a5958c
Leon Q. Brin <brinl1@southernct.edu>
2018-01-13 15:49:55 -0500

    Now wxmaxima fixed?

--- mathbuntu/.files/compileWxmaxima.sh
+++ mathbuntu/.files/compileWxmaxima.sh
@@ -6,8 +6,10 @@ nobody=$3
 
 # Make
 su $nobody -c "cd /tmp && tar -xzf $wxmaximaFile"
+su $nobody -c "cd /tmp/$wxmaximaDir && ./bootstrap"
 su $nobody -c "cd /tmp/$wxmaximaDir && ./configure"
 su $nobody -c "cd /tmp/$wxmaximaDir && make"
+su $nobody -c "cd /tmp/$wxmaximaDir && make allmo"
 
 # Install
 cd /tmp/$wxmaximaDir
